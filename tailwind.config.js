a9105687718ecbad458a76f789a02dd6442f32a0
elvincth <elvincth@gmail.com>
2022-02-08 12:14:22 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    container: {
+      padding: "1.5rem",
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
