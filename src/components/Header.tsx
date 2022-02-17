7eb65e9a0cc04045a19f2575b1e86bb6054bede3
BoGor918 <72458648+BoGor918@users.noreply.github.com>
2022-02-17 14:59:38 +0800

    Update

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SvgIcon from "./svg/SvgIcon";
+
+
+const Header = () => {
+  return (
+    <div>
+        <SvgIcon />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/createItem.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "~/components/Header";
+
+const createItem = () => {
+  return (
+    <div>
+      <Header />
+      <div>This is createItem</div>
+    </div>
+  );
+};
+
+export default createItem;
