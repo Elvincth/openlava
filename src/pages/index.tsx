ca92cd3835e26fdd6ff7ba50529307a29d84adc3
elvincth <elvincth@gmail.com>
2022-04-06 00:30:36 +0800

    done lisitng part also

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,34 @@ import Web3Modal from "web3modal";
 import { openLavaAddress } from "blockchain.config";
 import OpenLava from "artifacts/contracts/OpenLava.sol/OpenLava.json";
 
+const NFTCard = ({
+  src,
+  name,
+  description,
+}: {
+  src: string;
+  name: string;
+  description: string;
+}) => (
+  <div className="overflow-hidden transition duration-500 transform bg-white shadow-lg cursor-pointer w-80 rounded-xl hover:shadow-xl hover:scale-105">
+    <img
+      src={src}
+      className="object-cover h-[280px] w-[280px] mx-auto"
+      alt={name}
+    />
+    <div className="p-5">
+      <h1 className="text-2xl font-bold">{name}</h1>
+      <p className="mt-2 text-lg font-semibold text-gray-600">by John Smith</p>
+      <p className="mt-1 text-gray-500">{description}</p>
+
+      <div className="flex items-center mt-2">
+        <img className="w-3" src="eth.svg" alt="" />
+        <span className="ml-1">0.05</span>
+      </div>
+    </div>
+  </div>
+);
+
 const Home = () => {
   const [nfts, setNfts] = useState<any[]>([]);
   const [loadingState, setLoadingState] = useState("not-loaded");
@@ -83,7 +111,7 @@ const Home = () => {
 
   return (
     <div className="flex flex-col w-full h-screen">
-      <section className="flex flex-col lg:px-40 w-full lg:min-h-[580px] bg-cover  lg:mb-20 bg-[url('https://res.cloudinary.com/dasq4goqg/image/upload/v1645114580/Rectangle_461_amalkp.png')]">
+      <section className="flex flex-col lg:px-40 w-full lg:min-h-[580px] bg-cover lg:mb-10 bg-[url('https://res.cloudinary.com/dasq4goqg/image/upload/v1645114580/Rectangle_461_amalkp.png')]">
         <div className="flex flex-col justify-between h-full lg:flex-row">
           <div className="flex flex-col items-center justify-center pt-20 pb-12 pl-4 text-center lg:items-start lg:px-0 lg:text-left ">
             <h1 className="text-3xl font-bold lg:text-5xl">
@@ -108,33 +136,19 @@ const Home = () => {
         </div>
       </section>
 
-      <div className="items-center col-span-3 pt-8 pb-8 text-4xl font-bold text-center lg:pt-6">
+      <div className="items-center col-span-3 pt-8 pb-8 text-2xl font-bold text-center lg:text-4xl lg:pt-6">
         Art
       </div>
-      {/* map the data2 */}
-      <section className="grid self-center grid-cols-1 gap-20 pb-20 xl:grid-cols-3 md:grid-cols-2">
-        <div className="overflow-hidden transition duration-500 transform bg-white shadow-lg cursor-pointer w-80 rounded-xl hover:shadow-xl hover:scale-105">
-          <img
-            src="https://images.unsplash.com/photo-1577982787983-e07c6730f2d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=859&q=80"
-            alt=""
-          />
-          <div className="p-5">
-            <h1 className="text-2xl font-bold">El poder de la lampara</h1>
-            <p className="mt-2 text-lg font-semibold text-gray-600">
-              by John Smith
-            </p>
-            <p className="mt-1 text-gray-500">
-              Lorem ipsum carrots, enhanced undergraduate developer, but they do
-              occaecat time and vitality, Lorem ipsum carrots, enhanced
-              undergraduate developer, but they do occaecat time and vitality
-            </p>
 
-            <div className="flex items-center mt-2">
-              <img className="w-3" src="eth.svg" alt="" />
-              <span className="ml-1">0.05</span>
-            </div>
-          </div>
-        </div>
+      <section className="grid self-center grid-cols-1 gap-20 pb-20 xl:grid-cols-3 md:grid-cols-2">
+        {nfts.map((nft: any, i) => (
+          <NFTCard
+            key={i}
+            src={nft.image.replace("ipfs://", "https://nftstorage.link/ipfs/")}
+            name={nft.name}
+            description={nft.description}
+          />
+        ))}
       </section>
     </div>
   );
