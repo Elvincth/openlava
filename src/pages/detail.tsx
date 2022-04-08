d1da0d83f9a3faa1c8c44acda8734d3c257e3a17
tonywong586 <72515867+tonywong586@users.noreply.github.com>
2022-04-08 18:05:58 +0800

    Create Eth svg

--- src/pages/detail.tsx
+++ src/pages/detail.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Eth from '~/components/svg/Eth'
 
 const detail = () => {
     return (
@@ -7,7 +8,7 @@ const detail = () => {
                 <div className='flex flex-col max-w-[700px]'>
                     <div className='border border-gray-100 rounded-2xl'>
                         <div className='w-full p-4'>
-                            <svg width="15" height="20" viewBox="0 0 256 417" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid"><path fill="#343434" d="M127.961 0l-2.795 9.5v275.668l2.795 2.79 127.962-75.638z" /><path fill="#8C8C8C" d="M127.962 0L0 212.32l127.962 75.639V154.158z" /><path fill="#3C3C3B" d="M127.961 312.187l-1.575 1.92v98.199l1.575 4.6L256 236.587z" /><path fill="#8C8C8C" d="M127.962 416.905v-104.72L0 236.585z" /><path fill="#141414" d="M127.961 287.958l127.96-75.637-127.96-58.162z" /><path fill="#393939" d="M0 212.32l127.96 75.638v-133.8z" /></svg>
+                            <Eth />
                         </div>
                         <img className='rounded-b-2xl' src='https://static.boredpanda.com/blog/wp-content/uploads/2020/05/Illustrators-create-artwork-in-aid-of-health-workers-5eb48aea538d2__880.jpg' />
                     </div>
@@ -23,7 +24,7 @@ const detail = () => {
                         <h1 className=' font-normal text-xl pb-2'>Price</h1>
                         <div className='flex flex-row items-center'>
                             <div className='pb-1'>
-                                <svg width="25" height="30" viewBox="0 0 256 417" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid"><path fill="#343434" d="M127.961 0l-2.795 9.5v275.668l2.795 2.79 127.962-75.638z" /><path fill="#8C8C8C" d="M127.962 0L0 212.32l127.962 75.639V154.158z" /><path fill="#3C3C3B" d="M127.961 312.187l-1.575 1.92v98.199l1.575 4.6L256 236.587z" /><path fill="#8C8C8C" d="M127.962 416.905v-104.72L0 236.585z" /><path fill="#141414" d="M127.961 287.958l127.96-75.637-127.96-58.162z" /><path fill="#393939" d="M0 212.32l127.96 75.638v-133.8z" /></svg>
+                                <Eth />
                             </div>
                             <h1 className='text-4xl font-semibold px-2'>0.5</h1>
                             <h1 className='text-xl font-normal'>($1,646.45)</h1>
