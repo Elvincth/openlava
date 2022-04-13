b238a9f63abe4766d014296072173d1fe3342c0f
BoGor918 <boscocheung1234@gmail.com>
2022-04-13 22:08:55 +0800

    Openlava script comment

--- contracts/OpenLava.sol
+++ contracts/OpenLava.sol
@@ -9,23 +9,25 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "hardhat/console.sol";
 
 contract OpenLava is ERC721URIStorage {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
-    Counters.Counter private _itemsSold;
+    using Counters for Counters.Counter; // counter in the market place
+    Counters.Counter private _tokenIds; // token id counter for counting the number of token ids
+    Counters.Counter private _itemsSold; // Sold item counter for counting the number of sold items
 
-    address payable owner;
+    address payable owner; // nft owner
 
-    struct Nft {
-        uint256 itemId;
-        address payable seller;
-        address payable owner;
-        uint256 price;
+    // for defining the structure of each nft card
+    struct Nft { 
+        uint256 itemId; //the nft id
+        address payable seller; // the seller of the specific nft
+        address payable owner; // the person who bought the nft
+        uint256 price; // nft price 
         bool reserved; //is the token reserved?
     }
 
-    //pass in the id of the Nft and return the Nft
-    mapping(uint256 => Nft) private getNft;
+    // pass in the id of the Nft and return the Nft
+    mapping(uint256 => Nft) private getNft; 
 
+    // for defining the event strcture for meeting the NFT items to the blockchain
     event NftCreated(
         uint256 indexed itemId,
         address seller,
@@ -34,8 +36,9 @@ contract OpenLava is ERC721URIStorage {
         bool reserved
     );
 
-    constructor() ERC721("LAVA Tokens", "LAVA") {
-        owner = payable(msg.sender);
+    // the constructor for linking with the ERC721 
+    constructor() ERC721("LAVA Tokens", "LAVA") { 
+        owner = payable(msg.sender); // for getting the wallet address and the username of the owner user
     }
 
     //Create the token, and put the token on the marketplace
@@ -44,20 +47,21 @@ contract OpenLava is ERC721URIStorage {
         payable
         returns (uint256)
     {
-        _tokenIds.increment();
-        uint256 itemId = _tokenIds.current();
+        _tokenIds.increment(); // add the number of NFT items
+        uint256 itemId = _tokenIds.current(); // point the current token id for the creating item
 
-        _safeMint(msg.sender, itemId);
-        _setTokenURI(itemId, tokenURI);
+        _safeMint(msg.sender, itemId); // for user create the NFT item
+        _setTokenURI(itemId, tokenURI); // for creating the unique address of the NFT item
 
         //Now we create a new market item id
         require(price > 0, "Price must be at least 1 wei");
 
+        // getting the exact NFT by passing the item id
         getNft[itemId] = Nft(
             itemId,
-            payable(msg.sender),
-            payable(address(this)),
-            price,
+            payable(msg.sender), // the address that connect with the user
+            payable(address(this)), // contract address
+            price, // nft price
             false
         );
 
@@ -69,37 +73,90 @@ contract OpenLava is ERC721URIStorage {
         return itemId;
     }
 
-    //Allow the user to resell their purchased token
+    // Allow the user to resell their purchased token
     function resellToken(uint256 itemId, uint256 price) public payable {
-        address myAddress = address(this);
+        address myAddress = address(this); // storing the contract address
 
+        // the requirement for achieving the resell nft
         require(
-            getNft[itemId].owner == msg.sender,
-            "You are not the owner of this NFT"
+            getNft[itemId].owner == msg.sender, // if the user address is equal to the nft owner
+            "You are not the owner of this NFT" // else display the error message
         );
 
-        getNft[itemId].reserved = false;
-        getNft[itemId].price = price;
-        getNft[itemId].seller = payable(msg.sender);
-        getNft[itemId].owner = payable(myAddress);
-        _itemsSold.decrement();
+        getNft[itemId].reserved = false; 
+        getNft[itemId].price = price; // get the input price into the resell nft
+        getNft[itemId].seller = payable(msg.sender); // set the user address as the nft seller
+        getNft[itemId].owner = payable(myAddress); 
+        _itemsSold.decrement(); // minus the sold item in the marketplace, in other word, add back a nft back to marketplace
 
         _transfer(msg.sender, address(this), itemId);
     }
 
-    //Buy the token
+    // Buy the token
     function buyToken(uint256 itemId) public payable {
-        require(msg.value == getNft[itemId].price, "Invalid asking price!");
-        address seller = getNft[itemId].seller;
-        getNft[itemId].owner = payable(msg.sender);
-        getNft[itemId].reserved = true;
-        getNft[itemId].seller = payable(address(0));
-        _itemsSold.increment();
-        _transfer(address(this), msg.sender, itemId);
-        payable(seller).transfer(msg.value);
+        // requirement for achieving buy token
+        require(msg.value == getNft[itemId].price, "Invalid asking price!"); // if the ntf price is equal to the msg.value price
+
+        address seller = getNft[itemId].seller; // set the seller by using that nft item user address
+        getNft[itemId].owner = payable(msg.sender); // set the address of who buy the nft to the nft owner field
+        getNft[itemId].reserved = true; // nft is already sold 
+        getNft[itemId].seller = payable(address(0)); // set the nft seller by using that user address
+        _itemsSold.increment(); // increase the sold item
+        _transfer(address(this), msg.sender, itemId); 
+        payable(seller).transfer(msg.value); // pass the value price to transfer buyer ETH to seller
     }
 
-    //Get all the nft the user listed for sale
+    // Returns all unsold market items
+    function getMarketItems() public view returns (Nft[] memory) {
+        uint256 j = 0; // counter for counting the current item
+
+        Nft[] memory nfts = new Nft[](
+            _tokenIds.current() - _itemsSold.current()
+        );
+
+        // for loop listing all the unsold item
+        for (uint256 i = 0; i < _tokenIds.current(); i++) {
+            if (getNft[i + 1].owner == address(this)) {
+                uint256 currentId = i + 1;
+                Nft storage currentItem = getNft[currentId];
+                nfts[j] = currentItem;
+                j++;
+            }
+        }
+        return nfts; // return nft array
+    }
+
+    // Get the NFT by id
+    function getNftById(uint256 itemId) public view returns (Nft memory) {
+        return getNft[itemId]; // return the exact nft by passing the item id 
+    }
+
+    //  Listing the owned nft of the specific user
+    function getOwnedNfts() public view returns (Nft[] memory) {
+        uint256 j = 0; // counter for listing the nft
+        uint256 numOfNfts = 0; // counter of counting the number of nft
+
+        // counting how many nfts is owned by that specific user
+        for (uint256 i = 0; i < _tokenIds.current(); i++) {
+            if (getNft[i + 1].owner == msg.sender) { // if the nft owner is equal to user address
+                numOfNfts++; // add the number of owned nft
+            }
+        }
+
+        Nft[] memory nfts = new Nft[](numOfNfts); //Store all the nfts the user bought
+
+        // listing all owned nft
+        for (uint256 i = 0; i < _tokenIds.current(); i++) {
+            if (getNft[i + 1].owner == msg.sender) {
+                Nft storage nft = getNft[i + 1];
+                nfts[j] = nft;
+                j++;
+            }
+        }
+        return nfts;
+    }
+
+    // Get all the nft the user listed for sale
     function getListedNfts() public view returns (Nft[] memory) {
         uint256 j = 0;
         uint256 numOfNfts = 0; //Used to store how many items the user owned
@@ -117,65 +174,6 @@ contract OpenLava is ERC721URIStorage {
         //Store the nft to the array
         for (uint256 i = 0; i < _tokenIds.current(); i++) {
             if (getNft[i + 1].seller == msg.sender) {
-                Nft storage nft = getNft[i + 1];
-                nfts[j] = nft;
-                j++;
-            }
-        }
-        return nfts;
-    }
-
-    //Returns all unsold market items
-    function getMarketItems() public view returns (Nft[] memory) {
-        uint256 j = 0;
-
-        Nft[] memory nfts = new Nft[](
-            _tokenIds.current() - _itemsSold.current()
-        );
-
-        for (uint256 i = 0; i < _tokenIds.current(); i++) {
-            if (getNft[i + 1].owner == address(this)) {
-                uint256 currentId = i + 1;
-                Nft storage currentItem = getNft[currentId];
-                nfts[j] = currentItem;
-                j++;
-            }
-        }
-        return nfts;
-    }
-
-    //Get the NFT by id
-    function getNftById(uint256 itemId) public view returns (Nft memory) {
-        //require(getNft[itemId].reserved == false, "Item is not reserved");
-        return getNft[itemId];
-    }
-
-    // function getAll() public view returns (Nft[] memory) {
-    //     Nft[] memory nfts = new Nft[](_tokenIds.current());
-
-    //     for (uint256 i = 0; i < _tokenIds.current(); i++) {
-    //         nfts[i] = getNft[i];
-    //         i++;
-    //     }
-
-    //     return nfts;
-    // }
-
-    //Get the user owned nfts
-    function getOwnedNfts() public view returns (Nft[] memory) {
-        uint256 j = 0;
-        uint256 numOfNfts = 0;
-
-        for (uint256 i = 0; i < _tokenIds.current(); i++) {
-            if (getNft[i + 1].owner == msg.sender) {
-                numOfNfts++;
-            }
-        }
-
-        Nft[] memory nfts = new Nft[](numOfNfts); //Store all the nfts the user bought
-
-        for (uint256 i = 0; i < _tokenIds.current(); i++) {
-            if (getNft[i + 1].owner == msg.sender) {
                 Nft storage nft = getNft[i + 1];
                 nfts[j] = nft;
                 j++;
