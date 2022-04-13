3fd5a3bc374cc9d11ffbaad8faaa911057830aac
elvincth <elvincth@gmail.com>
2022-04-13 22:32:50 +0800

    fix

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,15 +10,15 @@ module.exports = {
     hardhat: {
       chainId: 1337,
     },
-    mumbai: {
-      //The projectId shd be hidden and store in .env, but for demo use only
-      url: "https://zmldxs5p79bt.usemoralis.com:2053/server",
-      accounts: [process.env.WALLET_PRIVATE_KEY],
-    },
-    mainnet: {
-      url: "https://axrmw0ees7iz.usemoralis.com:2053/server",
-      accounts: [process.env.WALLET_PRIVATE_KEY],
-    },
+    // mumbai: {
+    //   //The projectId shd be hidden and store in .env, but for demo use only
+    //   url: "https://zmldxs5p79bt.usemoralis.com:2053/server",
+    //   accounts: [process.env.WALLET_PRIVATE_KEY],
+    // },
+    // mainnet: {
+    //   url: "https://axrmw0ees7iz.usemoralis.com:2053/server",
+    //   accounts: [process.env.WALLET_PRIVATE_KEY],
+    // },
   },
   solidity: "0.8.4",
 };
